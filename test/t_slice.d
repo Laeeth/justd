476b8acee809864dc4ec10039e5df0b95159f520
Per Nordlöw <per.nordlow@gmail.com>
2014-04-08 23:05:13 +0200

    Fixes

--- test/t_slice.d
+++ test/t_slice.d
@@ -1,13 +1,14 @@
 #!/usr/bin/env rdmd-dev-module
 
-import std.stdio, std.algorithm, std.range, std.array;
+/* import std.stdio, std.algorithm, std.range, std.array; */
 
 void main(string[] args)
 {
     auto x = [1, 2, 3, 4]; // original
 
-    auto y = x[0..$]; // whole
-
     auto a = x[0..$ / 2]; // first part
     auto b = x[$ / 2.. $]; // second part
+
+    auto y = x[0..$]; // whole
+    auto z = x[0..$ + 1]; // error
 }
--- test/t_warn_unused_return.d
+++ test/t_warn_unused_return.d
@@ -2,7 +2,6 @@
 
 @safe pure nothrow void strictVoid(T)(T x) { }
 @safe pure nothrow bool strictBool(T)(T x) { return false; }
-
 @safe pure nothrow void nonstrictVoid(T)(ref T x) { x += 1; }
 @safe pure nothrow bool nonstrictBool(T)(ref T x) { x += 1; return false; }
 
