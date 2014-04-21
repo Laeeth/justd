3561280e5203f541557af21af06671992798e4a5
Per Nordlöw <per.nordlow@gmail.com>
2014-04-21 12:53:52 +0200

    Fix formattings

--- traits_ex.d
+++ traits_ex.d
@@ -40,52 +40,52 @@ alias lessThan = binaryFun!((a, b) => a < b);
 alias greaterThan = binaryFun!((a, b) => a > b);
 
 /* See also: http://d.puremagic.com/issues/show_bug.cgi?id=4427 */
-template isStruct(T)     { enum isStruct = is(T == struct ); }
+template isStruct(T) { enum isStruct = is(T == struct ); }
 unittest {
     interface I {}
     class A {}
     class B( T ) {}
     class C : B!int, I {}
     struct S {}
-    static assert( !isStruct!A );
-    static assert( !isStruct!( B!int ) );
-    static assert( !isStruct!C );
-    static assert( !isStruct!I );
-    static assert( isStruct!S );
-    static assert( !isStruct!int );
-    static assert( !isStruct!( int* ) );
+    static assert(!isStruct!A );
+    static assert(!isStruct!( B!int ) );
+    static assert(!isStruct!C );
+    static assert(!isStruct!I );
+    static assert(isStruct!S );
+    static assert(!isStruct!int );
+    static assert(!isStruct!( int* ) );
 }
 
-template isClass(T)      { enum isClass  = is(T == class ); }
+template isClass(T) { enum isClass = is(T == class ); }
 unittest {
     interface I {}
     class A {}
     class B( T ) {}
     class C : B!int, I {}
     struct S {}
-    static assert( isClass!A );
-    static assert( isClass!( B!int ) );
-    static assert( isClass!C );
-    static assert( !isClass!I );
-    static assert( !isClass!S );
-    static assert( !isClass!int );
-    static assert( !isClass!( int* ) );
+    static assert(isClass!A );
+    static assert(isClass!( B!int ) );
+    static assert(isClass!C );
+    static assert(!isClass!I );
+    static assert(!isClass!S );
+    static assert(!isClass!int );
+    static assert(!isClass!( int* ) );
 }
 
-template isInterface(T)  { enum isInterface = is(T == interface); }
+template isInterface(T) { enum isInterface = is(T == interface); }
 unittest {
     interface I {}
     class A {}
     class B( T ) {}
     class C : B!int, I {}
     struct S {}
-    static assert( !isInterface!A );
-    static assert( !isInterface!( B!int ) );
-    static assert( !isInterface!C );
-    static assert( isInterface!I );
-    static assert( !isInterface!S );
-    static assert( !isInterface!int );
-    static assert( !isInterface!( int* ) );
+    static assert(!isInterface!A );
+    static assert(!isInterface!( B!int ) );
+    static assert(!isInterface!C );
+    static assert(isInterface!I );
+    static assert(!isInterface!S );
+    static assert(!isInterface!int );
+    static assert(!isInterface!( int* ) );
 }
 
 template isType(T)       { enum isType = true; }
@@ -93,13 +93,13 @@ template isType(alias T) { enum isType = false; }
 
 unittest {
     struct S { alias int foo; }
-    static assert( isType!int );
-    static assert( isType!float );
-    static assert( isType!string );
-    //static assert( isType!S ); // Bugzilla 4431
-    static assert( isType!( S.foo ) );
-    static assert( !isType!4 );
-    static assert( !isType!"Hello world!" );
+    static assert(isType!int );
+    static assert(isType!float );
+    static assert(isType!string );
+    //static assert(isType!S ); // Bugzilla 4431
+    static assert(isType!( S.foo ) );
+    static assert(!isType!4 );
+    static assert(!isType!"Hello world!" );
 }
 
 /** Note that NotNull!T is not isNullable :) */
