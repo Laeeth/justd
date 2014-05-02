f4b22369625a435de8a463afb1238be15719c007
Per Nordlöw <per.nordlow@gmail.com>
2014-05-02 14:42:51 +0200

    More verbose namings

--- enums.d
+++ enums.d
@@ -5,12 +5,12 @@ import std.typetuple: allSatisfy;
 import std.traits: EnumMembers;
 import std.stdio: writefln;
 
-/** Join/Chain/Concatenate/Unite Enumerations $(D E).
+/** Chain (Join) Member Names of Enumerations $(D E).
     See also: http://forum.dlang.org/thread/f9vc6p$1b7k$1@digitalmars.com
 */
-template join(E...) if (allSatisfy!(isEnum, E)) {
+template chainMemberNames(E...) if (allSatisfy!(isEnum, E)) {
     mixin({
-            string r = "enum join { ";
+            string r = "enum chainMemberNames { ";
             foreach (T; E) {
                 import std.range: join;
                 r ~= [__traits(allMembers, T), " "].join(",");
@@ -18,24 +18,25 @@ template join(E...) if (allSatisfy!(isEnum, E)) {
             return r ~ "}";
         }());
 }
+alias chain = chainMemberNames;
 
 unittest
 {
     enum E1 { a, b, c }
     enum E2 { e, f, g }
     enum E3 { h, i, j}
-    alias E1_ = join!(E1);
-    alias E12 = join!(E1, E2);
-    alias E123 = join!(E1, E2, E3);
+    alias E1_ = chainMemberNames!(E1);
+    alias E12 = chainMemberNames!(E1, E2);
+    alias E123 = chainMemberNames!(E1, E2, E3);
     foreach (immutable e; [EnumMembers!E123])
         writefln("E123.%s: %d", e, e);
 }
 
-/** Unite Values of Enumerations $(D E).
+/** Unite Member Values of Enumerations $(D E).
  */
-template unite(E...) if (allSatisfy!(isEnum, E)) {
+template uniteMemberValues(E...) if (allSatisfy!(isEnum, E)) {
     mixin({
-            string r = "enum unite { ";
+            string r = "enum uniteMemberValues { ";
             foreach (T; E) {
                 import std.range: join;
                 r ~= [__traits(allMembers, T), " "].join(",");
@@ -43,12 +44,13 @@ template unite(E...) if (allSatisfy!(isEnum, E)) {
             return r ~ "}";
         }());
 }
+alias unite = uniteMemberValues;
 
 unittest
 {
     enum E1 { a = 0, b, c }
     enum E2 { d = E1.max+9, e, f }
-    alias E12 = unite!(E1, E2);
+    alias E12 = uniteMemberValues!(E1, E2);
     foreach (immutable e; [EnumMembers!E12])
         writefln("E12.%s: %d", e, e);
 }
