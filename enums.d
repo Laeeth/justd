28d75f9aac77fbb338c6f858c0bbbe6e82a6dd68
Per Nordlöw <per.nordlow@gmail.com>
2014-05-02 15:23:40 +0200

    Better namings

--- enums.d
+++ enums.d
@@ -10,54 +10,56 @@ import std.typetuple: allSatisfy;
 import std.traits: EnumMembers;
 import std.stdio: writefln;
 
-/** Chain (Join) Member Names of Enumerations $(D E).
+/** Unite (Chain, Join) Member Names of Enumerations $(D E).
     See also: http://forum.dlang.org/thread/f9vc6p$1b7k$1@digitalmars.com
     Todo: Nice error messages for name collisions.
 */
-template chainMemberNames(E...) if (allSatisfy!(isEnum, E)) {
+template uniteMemberNames(E...) if (allSatisfy!(isEnum, E)) {
     mixin({
-            string r = "enum chainMemberNames { ";
+            string r = "enum uniteMemberNames { ";
             foreach (T; E) {
                 import std.range: join;
-                r ~= [__traits(allMembers, T), " "].join(",");
+                r ~= [__traits(allMembers, T)].join(",") ~ ",";
             }
-            return r ~ "}";
+            return r ~ " }";
         }());
 }
-alias chain = chainMemberNames;
 
 unittest
 {
     enum E1 { a, b, c }
     enum E2 { e, f, g }
     enum E3 { h, i, j}
-    alias E1_ = chainMemberNames!(E1);
-    alias E12 = chainMemberNames!(E1, E2);
-    alias E123 = chainMemberNames!(E1, E2, E3);
+    alias E1_ = uniteMemberNames!(E1);
+    alias E12 = uniteMemberNames!(E1, E2);
+    alias E123 = uniteMemberNames!(E1, E2, E3);
     foreach (immutable e; [EnumMembers!E123])
         writefln("E123.%s: %d", e, e);
 }
 
-/** Unite Member Values of Enumerations $(D E).
+/** Unite (Chain, Join) Members (both their Names and Values) of Enumerations $(D E).
     Todo: Nice error messages for value collisions.
- */
-template uniteMemberValues(E...) if (allSatisfy!(isEnum, E)) {
+*/
+template uniteMembers(E...) if (allSatisfy!(isEnum, E)) {
     mixin({
-            string r = "enum uniteMemberValues { ";
+            string r = "enum uniteMembers { ";
             foreach (T; E) {
-                import std.range: join;
-                r ~= [__traits(allMembers, T), " "].join(",");
+                import std.range: map, join;
+                import std.conv: to;
+                r ~= [EnumMembers!T].map!(a =>
+                                          (to!string(a) ~ "=" ~
+                                           to!string(to!int(a)))).join(",") ~ ",";
             }
-            return r ~ "}";
+            return r ~ " }";
         }());
 }
-alias unite = uniteMemberValues;
 
 unittest
 {
     enum E1 { a = 0, b, c }
-    enum E2 { d = E1.max+9, e, f }
-    alias E12 = uniteMemberValues!(E1, E2);
-    foreach (immutable e; [EnumMembers!E12])
-        writefln("E12.%s: %d", e, e);
+    enum E2 { d = 10, e, f } // continue after E1
+    enum E3 { g = 20, h, i } // continue after E2
+    alias E = uniteMembers!(E1, E2, E3);
+    foreach (immutable e; [EnumMembers!E])
+        writefln("E.%s: %d", e, e);
 }
