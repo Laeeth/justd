f6f8a91c57f443ebf5ae6709da766d66a658125d
Per Nordlöw <per.nordlow@gmail.com>
2014-05-04 14:00:59 +0200

    Check assignment

--- enums.d
+++ enums.d
@@ -1,12 +1,23 @@
 #!/usr/bin/env rdmd
 
 /** Extension to enumerations.
-    TODO: Move to std.typecons (Type Constructor) in Phobos when ready.
-    TODO: Implement implicit conversions between EnumChain, EnumUnion and their
-    sources similar to Ada's subtype:
+
+    Copyright: Per Nordlöw 2014-.
+    License: $(WEB boost.org/LICENSE_1_0.txt, Boost License 1.0).
+    Authors: $(WEB Per Nordlöw)
+
+    TODO: Wrap EnumChain and EnumUnion in structs with
+    - alias this _EnumChain and _EnumUnion and
+    - that allow assignment from their parts and
+    - members that convert to their parts.
+
+    TODO: Alternatively: Implement implicit conversions between EnumChain,
+    EnumUnion and their sources similar to Ada's subtype:
     - Assignment to EnumUnion, EnumChain from its parts is always nothrow.
     - Assignment from EnumUnion, EnumChain to its parts may throw.
     when opImplicitCast is ready for use.
+
+    TODO: Move to std.typecons (Type Constructor) in Phobos when ready.
  */
 module enums;
 
@@ -14,8 +25,6 @@ import std.typetuple: allSatisfy, staticMap;
 import std.traits: EnumMembers, CommonType, OriginalType;
 import std.stdio: writeln, writefln;
 import std.conv: to;
-
-version = checkCollisions;
 
 /* Helpers */
 private enum isEnum(T) = is(T == enum);
@@ -25,7 +34,8 @@ private alias CommonOriginalType(T...) = CommonType!(staticMap!(OriginalType, T)
     All enumerator names of $(D E) must be unique.
     See also: http://forum.dlang.org/thread/f9vc6p$1b7k$1@digitalmars.com
 */
-template EnumChain(E...) if (allSatisfy!(isEnum, E) &&
+template EnumChain(E...) if (E.length >= 2 &&
+                             allSatisfy!(isEnum, E) &&
                              is(CommonOriginalType!(E)))
 {
     mixin({
@@ -47,12 +57,11 @@ template EnumChain(E...) if (allSatisfy!(isEnum, E) &&
 
 unittest
 {
-    enum E1 { a, b, c }
-    enum E2 { e, f, g }
-    enum E3 { h, i, j }
-    alias E1_ = EnumChain!(E1);
-    alias E12 = EnumChain!(E1, E2);
-    alias E123 = EnumChain!(E1, E2, E3);
+    enum E0 { a, b, c }
+    enum E1 { e, f, g }
+    enum E2 { h, i, j }
+    alias E12 = EnumChain!(E0, E1);
+    alias E123 = EnumChain!(E0, E1, E2);
     foreach (immutable e; [EnumMembers!E123])
         writefln("E123.%s: %d", e, e);
 }
@@ -60,7 +69,8 @@ unittest
 /** Unite (Join) Members (both their Names and Values) of Enumerations $(D E).
     All enumerator names and values of $(D E) must be unique.
  */
-template EnumUnion(E...) if (allSatisfy!(isEnum, E) &&
+template EnumUnion(E...) if (E.length >= 2 &&
+                             allSatisfy!(isEnum, E) &&
                              is(CommonOriginalType!(E)))
 {
     mixin({
@@ -73,14 +83,16 @@ template EnumUnion(E...) if (allSatisfy!(isEnum, E) &&
                     // name
                     enum n = to!string(m);
                     assert(n !in names,
-                           "Enumerator name " ~ T.stringof ~"."~n ~
+                           "Template argument E[" ~ to!string(ix)~
+                           "]'s enumerator name " ~ T.stringof ~"."~n ~
                            " collides with " ~ names[n] ~"."~n);
                     names[n] = T.stringof;
 
                     // value
                     enum v = to!O(m);
                     assert(v !in values,
-                           "Enumerator value of " ~ T.stringof ~"."~n ~" == "~ to!string(v) ~
+                           "Template argument E[" ~ to!string(ix)~
+                           "]'s enumerator value " ~ T.stringof ~"."~n ~" == "~ to!string(v) ~
                            " collides with member value of " ~ values[v]);
                     values[v] = T.stringof;
 
@@ -93,16 +105,65 @@ template EnumUnion(E...) if (allSatisfy!(isEnum, E) &&
 
 unittest
 {
-    enum E1:ubyte { a = 0, b = 3, c = 6 }
-    enum E2:ushort { p = 1, q = 4, r = 7 }
-    enum E3:uint { x = 2, y = 5, z = 8 }
-    alias E = EnumUnion!(E1, E2, E3);
-    /* writeln((OriginalType!E).stringof); */
-    foreach (immutable e; [EnumMembers!E])
-        writefln("E.%s: %s", e, to!(OriginalType!E)(e));
-
+    enum E0:ubyte  { a = 0, b = 3, c = 6 }
+    enum E1:ushort { p = 1, q = 4, r = 7 }
+    enum E2:uint   { x = 2, y = 5, z = 8 }
+    alias E = EnumUnion!(E0, E1, E2);
     enum D1 { a = 0, b = 3, c = 6 }
     enum D3 { x = 0, y = 3, z = 6 }
-    static assert(!__traits(compiles, { alias ED = EnumUnion!(E1, D1); } ));
-    static assert(!__traits(compiles, { alias ED = EnumUnion!(E1, D3); } ));
+    static assert(!__traits(compiles, { alias ED = EnumUnion!(E0, D1); } ));
+    static assert(!__traits(compiles, { alias ED = EnumUnion!(E0, D3); } ));
+}
+
+/** Instance Wrapper for EnumUnion.
+    Provides assignment from its sub enums.
+*/
+struct enumUnion(E...)
+{
+    alias U = EnumUnion!(E);    // Wrapped Type.
+    alias _value this;
+    /* TODO: Alternative to this set of static if? */
+    static if (E.length >= 1) {
+        void opAssign(E[0] e) { _value = cast(U)e; }
+        E[0] opCast(T : E[0])() const @safe pure nothrow {
+            bool match = false;
+            foreach (m; EnumMembers!U) {
+                if (m == _value) {
+                    match = true;
+                }
+            }
+            assert(match, "Cast failed");
+            return cast(E[0])_value;
+        }
+    }
+    static if (E.length >= 2) void opAssign(E[1] e) { _value = cast(U)e; }
+    static if (E.length >= 3) void opAssign(E[2] e) { _value = cast(U)e; }
+    static if (E.length >= 4) void opAssign(E[3] e) { _value = cast(U)e; }
+    static if (E.length >= 5) void opAssign(E[4] e) { _value = cast(U)e; }
+    static if (E.length >= 6) void opAssign(E[5] e) { _value = cast(U)e; }
+    static if (E.length >= 7) void opAssign(E[6] e) { _value = cast(U)e; }
+    static if (E.length >= 8) void opAssign(E[7] e) { _value = cast(U)e; }
+    static if (E.length >= 9) void opAssign(E[8] e) { _value = cast(U)e; }
+    private U _value;           // Instance.
+}
+unittest
+{
+    enum E0:ubyte  { a = 0, b = 3, c = 6 }
+    enum E1:ushort { p = 1, q = 4, r = 7 }
+    enum E2:uint   { x = 2, y = 5, z = 8 }
+
+    enumUnion!(E0, E1, E2) eu;
+
+    foreach (immutable e; [EnumMembers!(typeof(eu._value))])
+        writefln("E123.%s: %d", e, e);
+
+    E0 e0 = E0.max;
+    eu = e0;                    // checked at compile-time
+    assert(eu == E0.max);
+    writeln(eu);
+
+    e0 = cast(E0)eu;            // checked at run-time
+
+    enum Ex:uint { x = 2, y = 5, z = 8 }
+    static assert(!__traits(compiles, { Ex ex = Ex.max; eu = ex; } ));
 }
