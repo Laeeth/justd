b0a2f07b01a15801f14741a4e4518381b4e35676
Per Nordlöw <per.nordlow@gmail.com>
2014-07-04 12:50:22 +0200

    Use @trusted and add fun

--- dbg.d
+++ dbg.d
@@ -7,25 +7,31 @@
 */
 module dbg;
 
+@trusted:
+
 /* http://stackoverflow.com/questions/19413340/escaping-safety-with-debug-statements */
-debug auto trustedPureDebugCall(alias fn, A...) (A args) @trusted pure {
+debug auto trustedPureDebugCall(alias fn, A...) (A args) pure
+{
     debug return fn(args);
 }
 
-@trusted void debug_writeln(string file = __FILE__, uint line = __LINE__, T...)(T t) {
+void debug_writeln(string file = __FILE__, uint line = __LINE__, string fun = __FUNCTION__, T...)(T t)
+{
     import std.stdio: writeln;
-    writeln(file,":", line,":debug: ", t);
+    writeln(file, ":",line, "@",fun, ":debug: ", t);
 }
 alias debug_writeln dln;
 
-@trusted void debug_writefln(string file = __FILE__, uint line = __LINE__, T...)(T t) {
+void debug_writefln(string file = __FILE__, uint line = __LINE__, string fun = __FUNCTION__, T...)(T t)
+{
     import std.stdio: writefln;
-    writefln(file,":", line,":debug: ", t);
+    writefln(file, ":",line, "@",fun, ":debug: ", t);
 }
 alias debug_writefln dfln;
 
-@trusted void debug_variableln(alias T, string file = __FILE__, uint line = __LINE__)() {
+void debug_variableln(alias T, string file = __FILE__, uint line = __LINE__, string fun = __FUNCTION__)()
+{
     import std.stdio: writeln;
-    writeln(file,":", line,":debug: ", T.stringof, ":", T);
+    writeln(file, ":",line, "@",fun, ":debug: ", T.stringof, ":", T);
 }
 alias debug_variableln dvr;
