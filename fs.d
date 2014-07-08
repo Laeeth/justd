182e4dd069ba3b868b5e0f84ce1507adddfed4d9
Per Nordlöw <per.nordlow@gmail.com>
2014-07-08 21:39:51 +0200

    Add some more D defs

--- fs.d
+++ fs.d
@@ -1764,12 +1764,6 @@ class GStats
                                defaultStringDelims,
                                FileContent.sourceCode, FileKindDetection.equalsWhatsGiven);
 
-        enum keywordsD = [ "auto", "const", "double", "float", "int", "short", "struct",
-                           "unsigned", "break", "continue", "else", "for", "long",
-                           "switch", "void", "case", "default", "enum", "goto",
-                           "sizeof", "typedef", "volatile", "char", "do", "extern", "if",
-                           "return", "static", "union", "while", "class", "immutable", "import"];
-
         auto opersD = [
             // Arithmetic
             Op("+", OpArity.binary, OpAssoc.LR, 10*2, "Add"),
@@ -1838,6 +1832,30 @@ class GStats
                                   "gdmd"];
         auto magicForD = shebangLine(alt(lit("rdmd"),
                                          lit("gdmd")));
+
+        enum keywordsD = [`@property`, `@safe`, `@trusted`, `@system`, `@disable`, `abstract`, `alias`, `align`, `asm`, `assert`, `auto`, `body`, `bool`, `break`, `byte`, `case`, `cast`, `catch`,
+                          `cdouble`, `cent`, `cfloat`, `char`, `class`, `const`, `continue`, `creal`, `dchar`, `debug`, `default`, `delegate`, `delete`, `deprecated`,
+                          `do`, `double`, `else`, `enum`, `export`, `extern`, `false`, `final`, `finally`, `float`, `for`, `foreach`, `foreach_reverse`,
+                          `function`, `goto`, `idouble`, `if`, `ifloat`, `immutable`, `import`, `in`, `inout`, `int`, `interface`, `invariant`, `ireal`,
+                          `is`, `lazy`, `long`, `macro`, `mixin`, `module`, `new`, `nothrow`, `null`, `out`, `override`, `package`, `pragma`, `private`,
+                          `protected`, `public`, `pure`, `real`, `ref`, `return`, `scope`, `shared`, `short`, `static`, `struct`, `super`, `switch`,
+                          `synchronized`, `template`, `this`, `throw`, `true`, `try`, `typedef`, `typeid`, `typeof`, `ubyte`, `ucent`, `uint`, `ulong`,
+                          `union`, `unittest`, `ushort`, `version`, `void`, `volatile`, `wchar`, `while`, `with`, `__gshared`,
+                          `__thread`, `__traits`,
+                          `string`, `wstring`, `dstring`, `size_t`, `hash_t`, `ptrdiff_t`, `equals_`]; // aliases
+
+        enum builtinsD = [`toString`, `toHash`, `opCmp`, `opEquals`,
+                          `opUnary`, `opBinary`, `opApply`, `opCall`, `opAssign`, `opIndexAssign`, `opSliceAssign`, `opOpAssign`,
+                          `opIndex`, `opSlice`, `opDispatch`,
+                          `toString`, `toHash`, `opCmp`, `opEquals`, `Monitor`, `factory`, `classinfo`, `vtbl`, `offset`, `getHash`, `equals`, `compare`, `tsize`, `swap`, `next`, `init`, `flags`, `offTi`, `destroy`, `postblit`, `toString`, `toHash`,
+                          `factory`, `classinfo`, `Throwable`, `Exception`, `Error`, `capacity`, `reserve`, `assumeSafeAppend`, `clear`,
+                          `ModuleInfo`, `ClassInfo`, `MemberInfo`, `TypeInfo`];
+
+        enum propertiesD = [`sizeof`, `stringof`, `mangleof`, `nan`, `init`, `alignof`, `max`, `min`, `infinity`, `epsilon`, `mant_dig`, ``,
+                            `max_10_exp`, `max_exp`, `min_10_exp`, `min_exp`, `min_normal`, `re`, `im`];
+
+        enum specialsD = [`__FILE__`, `__LINE__`, `__DATE__`, `__EOF__`, `__TIME__`, `__TIMESTAMP__`, `__VENDOR__`, `__VERSION__`, `#line`];
+
         auto kindD = new FKind("D", [], ["d", "di"],
                                magicForD, 0,
                                [],
