e5d1e38d498dcbe30cbcda0035e87efc1557211c
Per Nordlöw <per.nordlow@gmail.com>
2014-08-15 00:33:43 +0200

    Use demangling

--- fs.d
+++ fs.d
@@ -596,7 +596,7 @@ void scanELF(NotNull!RegFile regfile,
                 if (doDemangle)
                 {
                     const hit = sym.demangleELF;
-                    writeln(hit[0], ": ", hit[1]);
+                    writeln(hit[0].toTag(), ": ", hit[1]);
                 }
                 else
                 {
