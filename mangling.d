0b3d16c415e0ea1fe150da9cf19ad51a789d1251
Per Nordlöw <per.nordlow@gmail.com>
2014-08-18 01:17:23 +0200

    Some more fixes

--- mangling.d
+++ mangling.d
@@ -1,9 +1,20 @@
 #!/usr/bin/env rdmd-dev
 
+// TODO: Use Nullable!(null)(string) to save space.
+
 /** Executable Symbol Name Mangling and Demangling. */
 module mangling;
 
 import std.range: empty;
+import std.algorithm: startsWith, findSplitAfter, skipOver, joiner;
+import std.typecons: tuple, Tuple, Nullable;
+import dbg;
+import algorithm_ex: split, splitBefore;
+import std.conv: to;
+import std.ascii: isDigit;
+import std.array: array;
+import std.stdio;
+import std.range: take, drop, front;
 
 /** Mangled Language. */
 enum Lang
@@ -37,9 +48,6 @@ unittest
     assert(toTag(Lang.java) == `Java`);
 }
 
-import std.typecons: tuple, Tuple, Nullable;
-import dbg;
-
 /** Instantiator for $(D Nullable).
     TODO: Add to Phobos.
 */
@@ -61,18 +69,67 @@ version(none) unittest
     auto x = nullable!(int.max)(3);
 }
 
-/** Decode C++ Type at $(D sym).
+/** Decode Unqualified C++ Type at $(D rest).
     See also: https://mentorembedded.github.io/cxx-abi/abi.html#mangling-type
 */
-string decodeCxxType(ref string sym)
+string decodeCxxUnqualifiedType(ref string rest)
 {
-    const biType = sym.decodeCxxBuiltinType;
+    const biType = rest.decodeCxxBuiltinType;
     if (!biType.isNull) { return biType.get; } // digest if match
 
-    const funType = sym.decodeCxxFunctionType;
+    const substType = rest.decodeCxxSubstitution;
+    if (!substType.isNull) { return substType.get; } // digest if match
+
+    const funType = rest.decodeCxxFunctionType;
     if (!funType.isNull) { return funType.get; } // digest if match
 
-    assert(false, "Unmatched encoding at " ~ sym);
+    assert(false, "Handle " ~ rest);
+}
+
+/** Decode C++ Type at $(D rest).
+    See also: https://mentorembedded.github.io/cxx-abi/abi.html#mangling-type
+*/
+string decodeCxxType(ref string rest)
+{
+    // <ref-qualifier>
+    bool isRef = false;      // & ref-qualifier
+    bool isRVRef = false;    // && ref-qualifier (C++11)
+    bool isComplexPair = false;    // complex pair (C 2000)
+    bool isImaginary = false;    // imaginary (C 2000)
+    bool isPointer = false;
+
+    /* TODO: Order of these may vary. */
+    const cvq = decodeCxxCVQualifiers(rest);
+    switch (rest[0])
+    {
+        case 'P': rest = rest[1..$]; isPointer = true; break;
+            // <ref-qualifier>: https://mentorembedded.github.io/cxx-abi/abi.html#mangle.ref-qualifier
+        case 'R': rest = rest[1..$]; isRef = true; break;
+        case 'O': rest = rest[1..$]; isRVRef = true; break;
+        case 'C': rest = rest[1..$]; isComplexPair = true; break;
+        case 'G': rest = rest[1..$]; isImaginary = true; break;
+        case 'U': rest = rest[1..$];
+            const sourceName = rest.decodeCxxSourceName;
+            dln("Handle vendor extended type qualifier <source-name>", rest);
+            break;
+        default: break;
+    }
+
+    string type;
+
+    // prefix qualifiers
+    if (cvq.isRestrict) { type ~= `restrict `; } // C99
+    if (cvq.isVolatile) { type ~= `volatile `; }
+    if (cvq.isConst) { type ~= `const `; }
+
+    type ~= rest.decodeCxxUnqualifiedType;
+
+    // suffix qualifiers
+    if (isRef) { type ~= `&`; }
+    if (isRVRef) { type ~= `&&`; }
+    if (isPointer) { type = type ~ `*`; }
+
+    return type;
 }
 
 /** Try to Decode C++ Operator at $(D rest).
@@ -149,14 +206,13 @@ Nullable!string decodeCxxOperator(ref string rest)
         case `cv`: type = `operator (<type>)`; break; // type-cast: TODO: Decode type
         case `li`: type = `operator "" <source-name>`; break;
             // TODO: case `v `: type = `operator <digit> <source-name>`; break;
-        default:
-            dln(`TODO: Handle `, rest);
+        default: break;
     }
     if (!type.isNull) { rest = rest[n..$]; } // digest if match
     return type;
 }
 
-/** Try to Decode C++ Builtin Type at $(D sym).
+/** Try to Decode C++ Builtin Type at $(D rest).
     See also: https://mentorembedded.github.io/cxx-abi/abi.html#mangle.builtin-type
 */
 Nullable!string decodeCxxBuiltinType(ref string rest)
@@ -219,11 +275,45 @@ Nullable!string decodeCxxBuiltinType(ref string rest)
             /* ::= u <source-name>	# vendor extended type */
 
         default:
-            dln(`TODO: Handle `, rest);
             break;
     }
 
     return type;
+}
+
+/** Decode C++ Substitution Type at $(D rest).
+    See also: https://mentorembedded.github.io/cxx-abi/abi.html#mangle.substitution
+*/
+Nullable!string decodeCxxSubstitution(ref string rest)
+{
+    if (rest.startsWith('S'))
+    {
+        string type;
+        rest = rest[1..$];
+        type ~= `::std::`;
+        switch (rest[0])
+        {
+            case 't': rest = rest[1..$]; type ~= `ostream`; break;
+            case 'a': rest = rest[1..$]; type ~= `allocator`; break;
+            case 'b': rest = rest[1..$]; type ~= `basic_string`; break;
+            case 's':
+                rest = rest[1..$];
+                type ~= `basic_string<char, std::char_traits<char>, std::allocator<char> >`;
+                break;
+            case 'i': rest = rest[1..$]; type ~= `istream`; break;
+            case 'o': rest = rest[1..$]; type ~= `ostream`; break;
+            case 'd': rest = rest[1..$]; type ~= `iostream`; break;
+            default:
+                dln(`Cannot handle C++ standard prefix character: '`, rest[0], `'`);
+                rest = rest[1..$];
+                break;
+        }
+        return nullable(type);
+    }
+    else
+    {
+        return typeof(return)();
+    }
 }
 
 /** Try to Decode C++ Function Type at $(D rest).
@@ -236,31 +326,63 @@ Nullable!string decodeCxxFunctionType(ref string rest)
     return type;
 }
 
-/** Demangle Symbol $(D sym) and Detect Language.
+struct CXXCVQualifiers
+{
+    bool isRestrict; // (C99)
+    bool isVolatile; // volatile
+    bool isConst; // const
+}
+
+/** Decode <CV-qualifiers>
+    See also: https://mentorembedded.github.io/cxx-abi/abi.html#mangle.CV-qualifiers
+*/
+CXXCVQualifiers decodeCxxCVQualifiers(ref string rest)
+{
+    CXXCVQualifiers cvq;
+    if (rest.startsWith('r')) { rest = rest[1..$]; cvq.isRestrict = true; }
+    if (rest.startsWith('V')) { rest = rest[1..$]; cvq.isVolatile = true; }
+    if (rest.startsWith('K')) { rest = rest[1..$]; cvq.isConst = true; }
+    return cvq;
+}
+
+/** Decode Identifier <source-name>.
+    See also: https://mentorembedded.github.io/cxx-abi/abi.html#mangle.source-name
+*/
+string decodeCxxSourceName(ref string rest)
+{
+    string id;
+    const match = rest.splitBefore!(a => !a.isDigit);
+    const digits = match[0];
+    if (!digits.empty)     // digit prefix
+    {
+        const num = to!int(digits);
+        rest = match[1];
+        id = rest[0..num]; // identifier, rest.take(num)
+        rest = rest[num..$]; // rest.drop(num);
+    }
+    return id;
+}
+
+/** Demangle Symbol $(D rest) and Detect Language.
     See also: https://en.wikipedia.org/wiki/Name_mangling
     See also: https://mentorembedded.github.io/cxx-abi/abi.html#mangling
     See also: https://gcc.gnu.org/onlinedocs/libstdc++/manual/ext_demangling.html
 */
-Tuple!(Lang, string) demangleSymbol(in string sym,
+Tuple!(Lang, string) demangleSymbol(string whole,
                                     string separator = null) /* @safe pure nothrow @nogc */
 {
-    import std.algorithm: startsWith, findSplitAfter, skipOver;
+    if (!whole.startsWith(`_`))
+    {
+        return tuple(Lang.c, whole); // assume C
+    }
 
-    const cxxHit = sym.findSplitAfter(`_Z`); // split into C++ prefix and rest
+    const cxxHit = whole.findSplitAfter(`_Z`); // split into C++ prefix and rest
 
     if (!cxxHit[0].empty) // C++
     {
         string[] ids; // TODO: Turn this into a range that is returned
         string rest = cxxHit[1];
         bool hasTerminator = false;
-
-        import algorithm_ex: split, splitBefore;
-        import std.algorithm: joiner;
-        import std.conv: to;
-        import std.ascii: isDigit;
-        import std.array: array;
-        import std.stdio;
-        import std.range: take, drop, front;
 
         if (rest.skipOver('N')) // nested name: https://mentorembedded.github.io/cxx-abi/abi.html#mangle.nested-name
         {
@@ -309,90 +431,19 @@ Tuple!(Lang, string) demangleSymbol(in string sym,
 
         // optional function arguments
         int argCount = 0;
-        char[] args;
-        if ((!rest.empty)) // if optional function arguments exist
+        char[] argTypes; // argument types
+        while (!rest.empty) // while optional function arguments exist
         {
-            args ~= `(`;
-
-            // <ref-qualifier>
-            bool isRef = false;      // & ref-qualifier
-            bool isRVRef = false;    // && ref-qualifier
-
-            // <CV-qualifiers>
-            bool isRestrict = false; // restrict
-            bool isConst = false; // const
-            bool isVolatile = false; // volatile
-
-            while (!rest.empty)
-            {
-                auto type = rest.decodeCxxBuiltinType;
-
-                switch (rest[0])
-                {
-                    // <CV-qualifiers>: https://mentorembedded.github.io/cxx-abi/abi.html#mangle.CV-qualifiers
-                    case 'r': rest = rest[1..$]; isRestrict = true; break;
-                    case 'V': rest = rest[1..$]; isVolatile = true; break;
-                    case 'K': rest = rest[1..$]; isConst = true; break;
-
-                        // <ref-qualifier>: https://mentorembedded.github.io/cxx-abi/abi.html#mangle.ref-qualifier
-                    case 'R': rest = rest[1..$]; isRef = true; break;
-                    case 'O': rest = rest[1..$]; isRVRef = true; break;
-
-                    case 'S': // standard namespace: std::
-                        rest = rest[1..$];
-                        type ~= `std::`;
-                        switch (rest[0])
-                        {
-                            case 't': rest = rest[1..$]; type ~= `ostream`; break;
-
-                            case 'a': rest = rest[1..$]; type ~= `allocator`; break;
-                            case 'b': rest = rest[1..$]; type ~= `basic_string`; break;
-                            case 's':
-                                rest = rest[1..$];
-                                type ~= `basic_string<char, std::char_traits<char>, std::allocator<char> >`;
-                                break;
-
-                            case 'i': rest = rest[1..$]; type ~= `istream`; break;
-                            case 'o': rest = rest[1..$]; type ~= `ostream`; break;
-                            case 'd': rest = rest[1..$]; type ~= `iostream`; break;
-
-                            default:
-                                /* dln(`Cannot handle C++ standard prefix character: '`, rest[0], `'`); */
-                                rest = rest[1..$];
-                                break;
-                        }
-                        break;
-                    default:
-                        /* dln(`Cannot handle character: '`, rest[0], `'`, ` in "`, rest, `"`); */
-                        rest = rest[1..$];
-                        break;
-                }
-
-                if (type)
-                {
-                    if (argCount >= 1)
-                    {
-                        args ~= ',';
-                    }
-
-                    if (isRestrict) { isRestrict = false; type = `restrict ` ~ type; } // C99
-                    if (isVolatile) { isVolatile = false; type = `volatile ` ~ type; }
-                    if (isConst) { isConst = false; type = `const ` ~ type; }
-
-                    if (isRef) { isRef = false; type ~= `&`; }
-                    if (isRVRef) { isRVRef = false; type ~= `&&`; }
-
-                    args ~= type;
-                    argCount += 1;
-                }
-            }
-            args ~= `)`;
+            const argType = rest.decodeCxxType;
+            if (argCount >= 1) { argTypes ~= `, `; }
+            argTypes ~= argType;
+            argCount += 1;
         }
 
         if (!separator)
             separator = `::`; // default C++ separator
 
-        const qid = to!string(ids.joiner(separator)) ~ to!string(args); // qualified id
+        const qid = to!string(ids.joiner(separator)) ~ `(` ~ to!string(argTypes) ~ `)`; // qualified id
         if (!rest.empty)
             dln(`rest: `, rest);
 
@@ -401,28 +452,34 @@ Tuple!(Lang, string) demangleSymbol(in string sym,
     else
     {
         import core.demangle: demangle;
-        const symAsD = sym.demangle;
+        const symAsD = whole.demangle;
         import std.conv: to;
-        if (symAsD != sym) // TODO: Why doesn't (symAsD is sym) work here?
+        if (symAsD != whole) // TODO: Why doesn't (symAsD is whole) work here?
             return tuple(Lang.d, to!string(symAsD));
         else
-            return tuple(Lang.init, sym);
+            return tuple(Lang.init, whole);
     }
 }
+
+import backtrace.backtrace;
 
 unittest
 {
     import assert_ex;
+    backtrace.backtrace.install(stderr);
 
-    assertEqual(`_ZStL19piecewise_construct`.demangleSymbol,
-                tuple(Lang.cxx, `std::piecewise_construct`));
+    /* assertEqual(`_ZStL19piecewise_construct`.demangleSymbol, */
+    /*             tuple(Lang.cxx, `std::piecewise_construct`)); */
 
     assertEqual(`_ZN9wikipedia7article8print_toERSo`.demangleSymbol,
-                tuple(Lang.cxx, `wikipedia::article::print_to(std::ostream&)`));
+                tuple(Lang.cxx, `wikipedia::article::print_to(::std::ostream&)`));
+
     assertEqual(`_ZN9wikipedia7article8print_toEOSo`.demangleSymbol,
-                tuple(Lang.cxx, `wikipedia::article::print_to(std::ostream&&)`));
+                tuple(Lang.cxx, `wikipedia::article::print_to(::std::ostream&&)`));
+
     assertEqual(`_ZN9wikipedia7article6formatEv`.demangleSymbol,
                 tuple(Lang.cxx, `wikipedia::article::format(void)`));
+
     assertEqual(`_ZN9wikipedia7article6formatE`.demangleSymbol,
                 tuple(Lang.cxx, `wikipedia::article::format`));
 }
