fed5ec253eddda33271b1f5d92593efb4c86b52a
Per Nordlöw <per.nordlow@gmail.com>
2014-08-18 15:07:34 +0200

    Add comment

--- typecons_ex.d
+++ typecons_ex.d
@@ -1,30 +1,30 @@
 module typecons_ex;
 
+// TODO: Add to Phobos and refer to http://forum.dlang.org/thread/lzyqywovlmdseqgqfvun@forum.dlang.org#post-ibvkvjwexdafpgtsamut:40forum.dlang.org
+
 import std.typecons: Nullable;
 
 /** Instantiator for $(D Nullable).
-    TODO: Add to Phobos and refer to http://forum.dlang.org/thread/lzyqywovlmdseqgqfvun@forum.dlang.org#post-ibvkvjwexdafpgtsamut:40forum.dlang.org
-*/
+ */
 auto nullable(T)(T a)
 {
     return Nullable!T(a);
 }
 unittest
 {
-    auto x = nullable(42.5);
+    auto x = 42.5.nullable;
     assert(is(typeof(x) == Nullable!double));
 }
 
 /** Instantiator for $(D Nullable).
-    TODO: Add to Phobos.
 */
 auto nullable(alias nullValue, T)(T value)
     if (is (typeof(nullValue) == T))
-    {
-        return Nullable!(T, nullValue)(value);
-    }
+{
+    return Nullable!(T, nullValue)(value);
+}
 unittest
 {
-    auto x = nullable!(int.max)(3);
+    auto x = 3.nullable!(int.max);
     assert(is (typeof(x) == Nullable!(int, int.max)));
 }
