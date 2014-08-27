ce66827bc7adb8b7322bafc9ee58ce7661b1bc9a
Per Nordlöw <per.nordlow@gmail.com>
2014-08-27 15:12:10 +0200

    Add min and max

--- memuse.d
+++ memuse.d
@@ -15,42 +15,55 @@ struct Linear1D(D, size_t ElementSize) {}
 /** Quadratic One-Dimensional Growth on index of type $(D D) with $(D ElementSize). */
 struct Quadratic1D(D, size_t ElementSize) {}
 
+import std.range: ElementType;
+import std.traits: isIntegral, isFloatingPoint, isNumeric, isIterable, isDynamicArray, isStaticArray, isArray, hasIndirections, isSomeString, isScalarType;
+import std.typecons: TypeTuple, Nullable;
+
 /** Get Asymptotic Memory Usage of $(D x) in Bytes.
  */
-template usageOf(T)
+template UsageOf(T)
 {
-    import std.range: ElementType;
-    import std.traits: isIntegral, isFloatingPoint, isNumeric, isIterable, isDynamicArray, isStaticArray, isArray, hasIndirections, isSomeString, isScalarType;
     static if (!hasIndirections!T)
     {
-        enum usageOf = T.sizeof;
+        enum UsageOf = T.sizeof;
     }
     else static if (isDynamicArray!T &&
                     isScalarType!(ElementType!T))
     {
-        alias usageOf = Linear1D!(size_t, ElementType!T.sizeof);
+        alias UsageOf = Linear1D!(size_t, ElementType!T.sizeof);
+    }
+    else static if (isDynamicArray!T &&
+                    isDynamicArray!(ElementType!T) &&
+                    isScalarType!(ElementType!(ElementType!T)))
+    {
+        alias UsageOf = Quadratic1D!(size_t, ElementType!T.sizeof);
     }
     else
     {
         static assert(false, "Type " ~ T.stringof ~ "unsupported.");
     }
+
+    /** Maybe Minimum Usage in bytes. */
+    /* size_t min() { return 0; } */
+
+    /** Maybe Maximum Usage in bytes. */
+    /* Nullable!size_t max() { return 0; } */
 }
 
 unittest
 {
-    import std.typecons: TypeTuple;
     foreach (T; TypeTuple!(byte, short, int, long,
                            ubyte, ushort, uint, ulong, char, wchar, dchar))
     {
-        static assert(usageOf!T == T.sizeof);
+        static assert(UsageOf!T == T.sizeof);
     }
 
     struct S { int x, y; }
-    static assert(usageOf!S == S.sizeof);
+    static assert(UsageOf!S == S.sizeof);
 
     foreach (T; TypeTuple!(byte, short, int, long))
     {
-        static assert(is(usageOf!(T[]) ==
+        static assert(is(UsageOf!(T[]) ==
                          Linear1D!(size_t, T.sizeof)));
     }
 }
