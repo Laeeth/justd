0fafde02050dd271fbeaa4c73d047984f4a57aaf
Per Nordlöw <per.nordlow@gmail.com>
2014-08-31 16:13:36 +0200

    Fix static asserts

---
+++ t_string.d
@@ -0,0 +1,20 @@
+#!/usr/bin/env rdmd-dev-module
+
+import std.stdio, std.algorithm, std.range, std.array;
+
+void main(string[] args)
+{
+    static assert([].ptr != null);
+    static assert("ab"[$..$].ptr == null);
+    static assert(![]);
+    static assert(!null);
+
+    static assert("ab"[0..0] == []);
+    static assert("ab"[$..$] == []);
+    static assert("ab"[$..$] !is null);
+    static assert("ab"[$..$] == null);
+    static assert("ab"[0..0] == "ab"[$..$]);
+    static assert("ab"[0..0]); // contextual hit (BOL)
+    static assert("a\nb"[2..2]); // contextual hit (beginning of second line)
+    static assert("ab"[$..$]); // contextual hit (EOL)
+}
