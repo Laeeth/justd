9167ef861801a7bccb6cee12174c7a63c399a7d6
Per Nordlöw <per.nordlow@gmail.com>
2014-09-06 16:20:03 +0200

    Use Solarized Light Color Theme

--- pprint.d
+++ pprint.d
@@ -156,6 +156,29 @@ enum VizForm
     Markdown,
 }
 
+/** See also: http://ethanschoonover.com/solarized */
+enum SolarizedLightColorTheme
+{
+    base00  = "657b83",
+    base01  = "586e75",
+    base02  = "073642",
+    base03  = "002b36",
+
+    base0   = "839496",
+    base1   = "93a1a1",
+    base2   = "eee8d5",
+    base3   = "fdf6e3",
+
+    yellow  = "b58900",
+    orange  = "cb4b16",
+    ed     = "dc322f",
+    magenta = "d33682",
+    viole   = "6c71c4",
+    blue    = "268bd2",
+    cya     = "2aa198",
+    gree    = "859900"
+}
+
 /** Visual Backend. */
 class Viz
 {
@@ -252,9 +275,8 @@ table {
     border-collapse: collapse;
 }
 tr:nth-child(even) { background-color: #EBEBEB; }
-tr:nth-child(3n+0) { background: #F8F8E8; }
-tr:nth-child(3n+1) { background: #F0F0F0; }
-tr:nth-child(3n+2) { background: #F8E8E8; }
+tr:nth-child(2n+0) { background: #` ~ SolarizedLightColorTheme.base2 ~ `; }
+tr:nth-child(2n+1) { background: #` ~ SolarizedLightColorTheme.base3 ~ `; }
 
 </style>
 </head>
