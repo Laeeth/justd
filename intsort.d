dcf26f84708f2e4836d45ce9ff71d22f83ee0e92
Per Nordlöw <per.nordlow@gmail.com>
2014-09-07 22:26:21 +0200

    Fix indents

--- intsort.d
+++ intsort.d
@@ -13,7 +13,8 @@ import std.traits: isUnsigned, isSigned, isIntegral, isFloatingPoint, Unsigned, 
 /** Biject (Shift) Signed $(D a) "up" to Unsigned (before radix sorting). */
 @trusted pure nothrow auto bijectToUnsigned(T)(T a) if (isNumeric!T)
 {
-    static if (isIntegral!T) {
+    static if (isIntegral!T)
+    {
         static      if (isSigned!T)
             return a + (cast(Unsigned!T)1 << (8*T.sizeof - 1)); // "add up""
         else static if (isUnsigned!T)
@@ -21,7 +22,8 @@ import std.traits: isUnsigned, isSigned, isIntegral, isFloatingPoint, Unsigned, 
         else
             static assert(false, "Unsupported integral input type " ~ T.stringof);
     }
-    else static if (isFloatingPoint!T) {
+    else static if (isFloatingPoint!T)
+    {
         static      if (is(T == float))
             return ff(*cast(uint*)(&a));
         else static if (is(T == double))
@@ -107,13 +109,18 @@ void radixSortImpl(R,
     /* Lookup number of radix bits from sizeof ElementType.
        These give optimal performance on Intel Core i7.
     */
-    static if (elemBits == 8) {
+    static if (elemBits == 8)
+    {
         enum radixNBits = 8;
-    } else static if (elemBits == 16 ||
-                      elemBits == 32 ||
-                      elemBits == 64) {
+    }
+    else static if (elemBits == 16 ||
+                    elemBits == 32 ||
+                    elemBits == 64)
+    {
         enum radixNBits = 16; // this prevents "rest" digit
-    } else {
+    }
+    else
+    {
         static assert("Cannot handle ElementType " ~ Elem.stringof);
     }
 
@@ -130,14 +137,17 @@ void radixSortImpl(R,
 
     alias U = typeof(bijectToUnsigned(x.front)); // Get Unsigned Integer Type of same precision as \tparam Elem.
 
-    if (nDigits != 1) {         // if more than one bucket sort pass (BSP)
+    if (nDigits != 1)           // if more than one bucket sort pass (BSP)
+    {
         doInPlace = false; // we cannot do in-place because each BSP is unstable and may ruin order from previous digit passes
     }
 
-    static if (false/* doInPlace */) {
+    static if (false/* doInPlace */)
+    {
         // Histogram Buckets Upper-Limits/Walls for values in \p x.
         Slice!size_t bins[radix] = void; // bucket slices
-        for (uint d = 0; d != nDigits; ++d) { // for each digit-index \c d (in base \c radix) starting with least significant (LSD-first)
+        for (uint d = 0; d != nDigits; ++d)  // for each digit-index \c d (in base \c radix) starting with least significant (LSD-first)
+        {
             const uint sh = d*radixNBits;   // digit bit shift
 
             // TODO: Activate and verify that performance is unchanged.
@@ -149,7 +159,8 @@ void radixSortImpl(R,
             // Populate Histogram \c O for current digit
             U ors  = 0;             // digits "or-sum"
             U ands = ~ors;          // digits "and-product"
-            for (size_t j = 0; j != n; ++j) { // for each element index \c j in \p x
+            for (size_t j = 0; j != n; ++j) // for each element index \c j in \p x
+            {
                 const uint i = (bijectToUnsigned(x[j], descending) >> sh) & mask; // digit (index)
                 ++bins[i].high();       // increase histogram bin counter
                 ors |= i; ands &= i; // accumulate bits statistics
@@ -161,7 +172,8 @@ void radixSortImpl(R,
             // Bin Boundaries: Accumulate Bin Counters Array
             size_t bin_max = bins[0].high();
             bins[0].low() = 0;                    // first floor is always zero
-            for (size_t j = 1; j != radix; ++j) { // for each successive bin counter
+            for (size_t j = 1; j != radix; ++j)  // for each successive bin counter
+            {
                 bin_max = max(bin_max, bins[j].high());
                 bins[j].low()  = bins[j - 1].high(); // previous roof becomes current floor
                 bins[j].high() += bins[j - 1].high(); // accumulate bin counter
@@ -172,11 +184,14 @@ void radixSortImpl(R,
              * Access \p x's elements in \em reverse to \em reuse filled caches from previous forward iteration.
              * \see \c in_place_indexed_reorder
              */
-            for (int r = radix - 1; r >= 0; --r) { // for each radix digit r in reverse order (cache-friendly)
-                while (bins[r]) {  // as long as elements left in r:th bucket
+            for (int r = radix - 1; r >= 0; --r) // for each radix digit r in reverse order (cache-friendly)
+            {
+                while (bins[r])  // as long as elements left in r:th bucket
+                {
                     const uint i0 = bins[r].pop_back(); // index to first element of permutation
                     Elem          e0 = x[i0]; // value of first/current element of permutation
-                    while (true) {
+                    while (true)
+                    {
                         const int rN = (bijectToUnsigned(e0, descending) >> sh) & mask; // next digit (index)
                         if (r == rN) // if permutation cycle closed (back to same digit)
                             break;
@@ -201,45 +216,56 @@ void radixSortImpl(R,
 
         import traits_ex: isEven;
 
-        foreach (d; 0..nDigits) { // for each digit-index \c d (in base \c radix) starting with least significant (LSD-first)
+        foreach (d; 0..nDigits) // for each digit-index \c d (in base \c radix) starting with least significant (LSD-first)
+        {
             const sh = d*radixNBits;   // digit bit shift
 
             // Reset Histogram Counters
             O[] = 0;
 
             // Populate Histogram \c O for current digit
-            static if (fastDigitDiscardal) {
+            static if (fastDigitDiscardal)
+            {
                 U ors  = 0;             // digits "or-sum"
                 U ands = ~ors;          // digits "and-product"
             }
-            for (size_t j = 0; j != n; ++j) { // for each element index \c j in \p x
+            for (size_t j = 0; j != n; ++j) // for each element index \c j in \p x
+            {
                 const uint i = (bijectToUnsigned(x[j], descending) >> sh) & mask; // digit (index)
                 ++O[i];              // increase histogram bin counter
-                static if (fastDigitDiscardal) {
+                static if (fastDigitDiscardal)
+                {
                     ors |= i; ands &= i; // accumulate bits statistics
                 }
             }
-            static if (fastDigitDiscardal) {
-                if ((! ors) || (! ~ands)) { // if bits in digit[d] are either all \em zero or all \em one
+            static if (fastDigitDiscardal)
+            {
+                if ((! ors) || (! ~ands)) // if bits in digit[d] are either all \em zero or all \em one
+                {
                     continue;               // no sorting is needed for this digit
                 }
             }
 
             // Bin Boundaries: Accumulate Bin Counters Array
-            for (size_t j = 1; j != radix; ++j) { // for each successive bin counter
+            for (size_t j = 1; j != radix; ++j) // for each successive bin counter
+            {
                 O[j] += O[j - 1]; // accumulate bin counter
             }
 
             // Reorder. Access \p x's elements in \em reverse to \em reuse filled caches from previous forward iteration.
             // \em Stable Reorder From \p x to \c y using Normal Counting Sort (see \c counting_sort above).
-            for (size_t j = n - 1; j < n; --j) { // for each element \c j in reverse order. when j wraps around j < n is no longer true
+            for (size_t j = n - 1; j < n; --j) // for each element \c j in reverse order. when j wraps around j < n is no longer true
+            {
                 const uint i = (bijectToUnsigned(x[j], descending) >> sh) & mask; // digit (index)
                 y[--O[i]] = x[j]; // reorder into y
             }
 
-            static if (nDigits.isEven) { // if even number of passes
+            static if (nDigits.isEven) // if even number of passes
+            {
                 swap(x, y);
-            } else {
+            }
+            else
+            {
                 x[] = y[];
             }
         }
@@ -315,7 +341,8 @@ unittest {
 
     import std.typetuple: TypeTuple;
     int n = 1000_000;
-    foreach (ix, T; TypeTuple!(byte, short, int, long)) {
+    foreach (ix, T; TypeTuple!(byte, short, int, long))
+    {
         test!T(n); // test signed
         test!(Unsigned!T)(n); // test unsigned
     }
