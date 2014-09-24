fc69ac0bbf35c43a02516142e7fef860f7c9e653
Per Nordlöw <per.nordlow@gmail.com>
2014-09-24 22:09:59 +0200

    Add see also

--- t_rcstring_array.d
+++ t_rcstring_array.d
@@ -3,6 +3,7 @@
 import std.container: Array;
 import rcstring;
 
+/** See also: http://forum.dlang.org/thread/lv5iob$f9h$1@digitalmars.com?page=14#post-izjwveedmaxqyiklzlas:40forum.dlang.org */
 unittest
 {
     Array!RCString x;
