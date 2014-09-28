eaff1161d518d2a912f46121747bc3b9f5d29161
Per Nordlöw <per.nordlow@gmail.com>
2014-09-28 18:18:04 +0200

    Move test program to cn5query

---
+++ cn5query/source/app.d
@@ -0,0 +1,39 @@
+import std.stdio;
+import conceptnet5;
+
+int main(string[] args)
+{
+    auto net = new Net!(true, false)(`~/Knowledge/conceptnet5-5.3/data/assertions/`);
+    net.showConcepts(`car`);
+    net.showConcepts(`car_wash`);
+
+    while (true)
+    {
+        write(`Lookup: `); stdout.flush;
+        string line;
+        if ((line = readln()) !is null)
+        {
+            import std.string: strip;
+            net.showConcepts(line.strip);
+        }
+        else
+        {
+            break;
+        }
+    }
+    /* if (true) */
+    /* { */
+    /*     auto netPack = net.pack; */
+    /*     writeln(`Packed to `, netPack.length, ` bytes`); */
+    /* } */
+
+    if (false) // just to make all variants of compile
+    {
+        /* auto netH = new Net!(!useHashedStorage)(`~/Knowledge/conceptnet5-5.3/data/assertions/`); */
+    }
+
+    write(`Press enter to continue: `);
+    readln();
+
+    return 0;
+}
--- conceptnet5.d
+++ conceptnet5.d
@@ -1,4 +1,4 @@
-#!/usr/bin/env rdmd-dev
+#!/usr/bin/env rdmd-dev-module
 
 /** ConceptNet 5 Commonsense Knowledge Database.
 
@@ -841,43 +841,4 @@ class Net(bool useArray = true,
     {
         // assert(!new.contextOf([`gun`, `mask`, `money`, `caught`, `stole`]).find(`robbery`).empty);
     }
-}
-
-version(none) unittest
-{
-    import std.stdio: stderr;
-    // TODO Add auto-download and unpack from http://conceptnet5.media.mit.edu/downloads/current/
-
-    auto net = new Net!(true, false)(`~/Knowledge/conceptnet5-5.3/data/assertions/`);
-    net.showConcepts(`car`);
-    net.showConcepts(`car_wash`);
-
-    while (true)
-    {
-        write(`Lookup: `); stdout.flush;
-        string line;
-        if ((line = readln()) !is null)
-        {
-            net.showConcepts(line);
-        }
-        else
-        {
-            break;
-        }
-    }
-    /* if (true) */
-    /* { */
-    /*     auto netPack = net.pack; */
-    /*     writeln(`Packed to `, netPack.length, ` bytes`); */
-    /* } */
-
-    if (false) // just to make all variants of compile
-    {
-        /* auto netH = new Net!(!useHashedStorage)(`~/Knowledge/conceptnet5-5.3/data/assertions/`); */
-    }
-
-    write(`Press enter to continue: `);
-    readln();
-
-    return 0;
 }
