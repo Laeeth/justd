e8ce84ee849044023594afb696b0bad5cec4354d
Per Nordlöw <per.nordlow@gmail.com>
2014-10-04 21:18:33 +0200

    Simplify

--- t_array_msgpack.d
+++ t_array_msgpack.d
@@ -1,23 +1,11 @@
 #!/usr/bin/env rdmd-dev-module
 
-import std.stdio;
-import std.conv: to;
 import msgpack;
 import std.container;
-import std.traits;
-
-import backtrace.backtrace;
 
 unittest
 {
-    import std.stdio: stderr;
-    backtrace.backtrace.install(stderr);
-
-    /* registerPackHandler!(Array!string, arrayPackHandler); */
-
     Array!string x = ["x", "y"];
-    writeln(x.pack);
-
     string[] y = ["x", "y"];
-    writeln(y.pack);
+    assert(x.pack == y.pack);
 }
