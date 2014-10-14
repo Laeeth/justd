29d870da7d511f47e2c451d324f1082faeac1ffa
Per Nordlöw <per.nordlow@gmail.com>
2014-10-14 15:45:35 +0200

    Add test

--- t_splitter.d
+++ t_splitter.d
@@ -2,6 +2,14 @@
 
 import std.stdio, std.algorithm;
 
-void main(string[] args) {
+void main(string[] args)
+{
     writeln(splitter("hello world", ' '));
+    import std.uni: isWhite;
+    import std.algorithm: splitter;
+    import std.conv: to;
+    auto line = " car_wash ";
+    import std.string: strip;
+    auto normalizedLine = line.strip.splitter!isWhite.joiner("_").to!string;
+    writeln(normalizedLine);
 }
