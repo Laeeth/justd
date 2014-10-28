89cafc79922d954e92199fda84ab9e5cc25ca914
Per Nordlöw <per.nordlow@gmail.com>
2014-10-28 16:37:23 +0100

    Fix

---
+++ t_opIn.d
@@ -0,0 +1,71 @@
+import std.stdio;
+
+struct Set(T)
+{
+    static if (is(T == char))
+        private string _data;
+    else
+        private T[] _data;
+public:
+    typeof(this) opSlice(char lo, char hi)
+    {
+        Set result;
+        foreach(c; lo .. hi)
+            result._data ~= c;
+        return result;
+    }
+    typeof(this) opSlice(char lohi)
+    {
+        Set result;
+        result._data ~= lohi;
+        return result;
+    }
+    bool opIn_r(char elem)
+    {
+        import std.range: empty;
+        if (_data.empty)
+            return false;
+        return ((elem >= _data[0]) &&
+                (elem <= _data[$-1]));
+    }
+    static if (is(T == char))
+    {
+        string toString()
+        {
+            return _data;
+        }
+    }
+}
+
+import std.typecons: CommonType;
+
+/** Instantiator for $(D Set). */
+Set!(CommonType!T) set(T...)(T args)
+{
+    return typeof(return)();
+}
+
+unittest
+{
+    assert(1 in set(1, 2, 3));
+}
+
+void main(string[] args)
+{
+    Set!char cs;
+
+    auto a2k = cs['a' .. 'k'+1];
+    auto A2K = cs['A' .. 'K'+1];
+    auto Z29 = cs['0' .. '9'+1];
+
+    assert('a' in a2k);
+    assert(!('x' in a2k));
+
+    assert('A' in A2K);
+    assert(!('X' in A2K));
+
+    import std.conv;
+    assert(to!string(Z29) == "0123456789");
+
+    assert('x' in cs['x'..'x'+1]);
+}
