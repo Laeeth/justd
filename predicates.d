cd9eec4ce55301007f0af6ce263a86f5ec5a69ce
Per Nordlöw <per.nordlow@gmail.com>
2014-10-28 13:16:22 +0100

    Add range variant

--- predicates.d
+++ predicates.d
@@ -8,6 +8,7 @@
 module predicates;
 
 import std.traits: CommonType;
+import std.range: isIterable, ElementType;
 
 /** Return true if $(D x) is a equal to any of $(D y).
     TODO Make ys lazy if any of ys is a delegate.
@@ -17,19 +18,48 @@ bool of(S, T...)(in S x, in T ys) pure if (ys.length >= 1 &&
                                                        CommonType!(T).init; })))
 {
     foreach (y; ys)
-    {
         if (x == y)
-        {
             return true;
-        }
-    }
     return false;
 }
-
-alias isEither = of;
 
 @safe pure @nogc nothrow unittest
 {
     assert(1.of(1, 2, 3));
     assert(!4.of(1, 2, 3));
 }
+
+/** Return true if $(D x) is a equal to any of $(D y).
+*/
+bool of(E, R)(E x, R ys) pure if (isIterable!R/*  && */
+                                  /* is(E.init == ElementType!R.init) */)
+{
+    import std.range: isNarrowString;
+    static if (isNarrowString!R)
+    {
+        import std.algorithm: canFind;
+        return ys.canFind(x);
+    }
+    else
+    {
+        foreach (y; ys)
+            if (x == y)
+                return true;
+        return false;
+    }
+}
+
+@safe pure nothrow unittest
+{
+    assert(1.of([1, 2, 3]));
+    assert(!4.of([1, 2, 3]));
+}
+
+@safe pure unittest
+{
+    assert('a'.of("abc"));
+    assert(!'z'.of("abc"));
+    assert('å'.of("åäö"));
+}
+
+alias isEither = of;
