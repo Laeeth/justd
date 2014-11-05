07141d5069595c61202bb028288c319e1e87b15d
Per Nordlöw <per.nordlow@gmail.com>
2014-11-05 01:18:08 +0100

    Use backtrace only in debug

--- knetquery/source/app.d
+++ knetquery/source/app.d
@@ -1,11 +1,11 @@
 import std.stdio;
 import knet;
-import backtrace.backtrace;
+debug import backtrace.backtrace;
 
 int main(string[] args)
 {
-    import std.stdio: stderr;
-    backtrace.backtrace.install(stderr);
+    debug import std.stdio: stderr;
+    debug backtrace.backtrace.install(stderr);
 
     auto net = new Net!(true, false)(`~/Knowledge/conceptnet5-5.3/data/assertions/`);
     net.showConcepts(`car_wash`);
