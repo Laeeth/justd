ecd89cebfc76e28c4f09c0c965a8681eb1f034d6
Per Nordlöw <per.nordlow@gmail.com>
2014-11-24 22:44:01 +0100

    Make printers @safe nothrow pure

--- dbg.d
+++ dbg.d
@@ -15,23 +15,28 @@ debug auto trustedPureDebugCall(alias fn, A...) (A args) pure
     debug return fn(args);
 }
 
+@safe pure nothrow:
+
 void debug_writeln(string file = __FILE__, uint line = __LINE__, string fun = __FUNCTION__, T...)(T t)
 {
     import std.stdio: writeln;
-    writeln(file, ":",line, ":"/* , ": in ",fun */, " debug: ", t);
+    try { debug writeln(file, ":",line, ":"/* , ": in ",fun */, " debug: ", t); }
+    catch (Exception) { }
 }
 alias dln = debug_writeln;
 
 void debug_writefln(string file = __FILE__, uint line = __LINE__, string fun = __FUNCTION__, T...)(T t)
 {
     import std.stdio: writefln;
-    writefln(file, ":",line, ":"/* , ": in ",fun */, " debug: ", t);
+    try { debug writefln(file, ":",line, ":"/* , ": in ",fun */, " debug: ", t); }
+    catch (Exception) { }
 }
 alias dfln = debug_writefln;
 
 void debug_variableln(alias T, string file = __FILE__, uint line = __LINE__, string fun = __FUNCTION__)()
 {
     import std.stdio: writeln;
-    writeln(file, ":",line, ":" /* , ": in ",fun */, "  debug: ", T.stringof, ":", T);
+    try { debug writeln(file, ":",line, ":" /* , ": in ",fun */, "  debug: ", T.stringof, ":", T); }
+    catch (Exception) { }
 }
 alias dvr = debug_variableln;
