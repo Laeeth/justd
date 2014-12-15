1ec3b00e6926fda58f6582b4e602163871be760c
Per Nordlöw <per.nordlow@gmail.com>
2014-12-16 00:09:39 +0100

    More verbose template parameter namings

--- range_ex.d
+++ range_ex.d
@@ -545,10 +545,10 @@ static if (__VERSION__ < 2067)
 /* Return Array of Key-Value Pairs of Associative Array $(D aa).
    See also: http://forum.dlang.org/thread/dxotcrutrlmszlidufcr@forum.dlang.org?page=2#post-fhkgitmifgnompkqiscd:40forum.dlang.org
 */
-auto pairs(K,V)(V[K] aa)
+auto pairs(Key,Value)(Value[Key] aa)
 {
     import std.typecons: Tuple, tuple;
-    Tuple!(K,V)[] arr;
+    Tuple!(Key,Value)[] arr;
     arr.reserve(aa.length);
     foreach (key; aa.byKey)
     {
