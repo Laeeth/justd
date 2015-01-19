4c67b17bfbe2d26000424ea5660e21267615f693
Per Nordlöw <per.nordlow@gmail.com>
2015-01-19 10:13:34 +0100

    Add test for enum

--- t_bitfields.d
+++ t_bitfields.d
@@ -4,31 +4,37 @@ import std.stdio, std.algorithm, std.bitmanip;
 
 struct Bits(int N) if(N > 0 && N <= 32) {}
 
-private struct Struct {
+private struct S
+{
     @Bits!3 ubyte bits3;
     @Bits!1 ubyte bits1;
     @Bits!4 uint bits4;
     ubyte bits8; //no UDA necessary
 }
 
-void main(string[] args) {
-    alias dln = writeln;
-    dln(Struct.sizeof);
-    immutable bf = bitfields!(uint, "x",
-                              2,
-                              int, "y",
-                              3,
-                              uint, "z",
-                              2,
-                              bool, "flag", 1);
-    struct A {
+enum E2 { a, b, c, d }
+
+void main(string[] args)
+{
+    writeln("S.sizeof: ", S.sizeof);
+
+    immutable bf = bitfields!(uint, "x", 2,
+                              int, "y", 2,
+                              uint, "z", 2,
+                              E2, "e2", 2);
+
+    struct A
+    {
         int a;
         mixin(bf);
     }
+
+
     A obj;
     obj.a = 11;
     obj.x = 2;
     obj.z = obj.x;
-    dln(obj);
-    dln(bf);
+
+    writeln(obj);
+    writeln(bf);
 }
