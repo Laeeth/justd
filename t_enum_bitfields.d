a7eeca61bbd05ff10e28fb3e7a27a949d10e9451
Per Nordlöw <per.nordlow@gmail.com>
2015-01-19 10:38:07 +0100

    Fix

--- t_enum_bitfields.d
+++ t_enum_bitfields.d
@@ -4,32 +4,22 @@ import std.stdio, std.bitmanip;
 
 enum E2 { a, b, c, d, e }
 
+immutable bf = bitfields!(uint, "x", 6,
+                          E2, "e2", 2);
+
+struct A { mixin(bf); }
+
 void main(string[] args)
 {
-    import std.exception: assertThrown;
-    import core.exception: AssertError;
-
-    immutable bf = bitfields!(uint, "x", 2,
-                              int, "y", 2,
-                              uint, "z", 2,
-                              E2, "e2", 2);
-
-    struct A
-    {
-        mixin(bf);
-    }
-
     A obj;
     obj.x = 2;
-    obj.y = 1;
-    obj.z = obj.x;
     obj.e2 = E2.a;
+
+    import core.exception: AssertError;
     try
     {
         obj.e2 = E2.e;
         assert(false, "Exception not caught");
     }
-    catch (core.exception.AssertError e)
-    {
-    }
+    catch (core.exception.AssertError e) { /* ok to throw */ }
 }
