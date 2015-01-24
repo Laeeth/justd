6a3af19cc2a214a04bbc299f0227d40f4a69124c
Per Nordlöw <per.nordlow@gmail.com>
2015-01-24 14:13:24 +0100

    Simplify

--- t_arrayOpAppend.d
+++ t_arrayOpAppend.d
@@ -1,14 +1,14 @@
 #!/usr/bin/env rdmd-dev-module
 
-import std.stdio, std.algorithm, std.range;
-
 unittest
 {
+    import std.stdio, std.algorithm, std.range;
     import std.container: Array;
 
     alias E = int; // element type
     alias K = string; // key type
     alias A = Array!E; // array type
+
     A[K] x;
 
     // x["a"] = A.init; // this line prevents RangeError
