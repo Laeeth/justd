70ca5c0f8fd811e731712b73b70097e211875841
Per Nordlöw <per.nordlow@gmail.com>
2015-01-30 19:22:48 +0100

    Add more static asserts

--- t_arraySlicing.d
+++ t_arraySlicing.d
@@ -1,6 +1,29 @@
 void main()
 {
-    const x = [1, 2, 3];
-    const y = x[];
-    const z = x[2 .. 1];
+    const x = [1, 2, 3, 4];
+
+    const x4 = x[$ .. $*3];
+    const x1 = x[1 + $ .. $ + 1];
+    const x3 = x[$ - 1 .. $];
+
+    static assert(!__traits(compiles, { const E = x[$     .. $*3]; }));
+    static assert(!__traits(compiles, { const E = x[$     .. 1 + $]; }));
+    static assert(!__traits(compiles, { const E = x[$     .. $ + 1]; }));
+    static assert(!__traits(compiles, { const E = x[0     .. $ + 1]; }));
+    static assert(!__traits(compiles, { const E = x[$ + 1 .. $ + 1]; }));
+    static assert(!__traits(compiles, { const E = x[$*3/2 .. $/2]; }));
+
+    const r = x[2*$/3 .. 3*$/4];
+    const q = x[$*2/3 .. $*3/4];
+
+    const f = x[0 + 0 .. $/2]; // first half
+    const s = x[$/2 .. $]; // second half
+    const w = x[0 .. $]; // whole
+    const b = x[0 .. 0]; // beginning
+    const e = x[$ .. $]; // end
+
+    const y = x[1 - 1 .. $/2]; // first half
+
+    const t = x[$/3 .. $/2];
+    const u = x[$/(4 - 1) .. $/2];
 }
