9b2c2d50e5aea838ddc0958c1e49949959437744
Per Nordlöw <per.nordlow@gmail.com>
2015-02-01 16:28:50 +0100

    Add more checks

--- t_arraySlicing.d
+++ t_arraySlicing.d
@@ -4,11 +4,13 @@ void main()
 
     const x4 = x[-1 .. 1];
     const x4 = x[-1 .. $*3];
+    const x5 = x[0 ..- $*3];
     const x4 = x[$ .. $*3];
     const x1 = x[1 + $ .. $ + 1];
     const x3 = x[$ - 1 .. $];
 
     static assert(!__traits(compiles, { const E = x[-1 .. 0]; }));
+    static assert(!__traits(compiles, { const E = x[0 .. -$]; }));
     static assert(!__traits(compiles, { const E = x[-1 .. $]; }));
     static assert(!__traits(compiles, { const E = x[$     .. $*3]; }));
     static assert(!__traits(compiles, { const E = x[$     .. 1 + $]; }));
