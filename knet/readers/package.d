2a6339e1c005bc6bc0bfc9d0864cd3dc7ff630a8
Per Nordlöw <per.nordlow@gmail.com>
2015-02-11 19:07:16 +0100

    Add package.d

---
+++ knet/readers/package.d
@@ -0,0 +1,9 @@
+module knet.readers;
+
+public import knet.readers.moby;
+public import knet.readers.swesaurus;
+public import knet.readers.synlex;
+public import knet.readers.nell;
+public import knet.readers.cn5;
+public import knet.readers.folklex;
+public import knet.readers.wordnet;
