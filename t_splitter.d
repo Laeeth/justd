ee2226d0ea1a5dd3342b1ed4fee5fc42e3804cc1
Per Nordlöw <per.nordlow@gmail.com>
2015-02-13 12:32:31 +0100

    Add fixes

--- t_splitter.d
+++ t_splitter.d
@@ -5,11 +5,23 @@ import std.stdio, std.algorithm;
 void main(string[] args)
 {
     writeln(splitter("hello world", ' '));
+
     import std.uni: isWhite;
     import std.algorithm: splitter;
     import std.conv: to;
+    import std.array: array;
+
     auto line = " car_wash ";
     import std.string: strip;
     auto normalizedLine = line.strip.splitter!isWhite.joiner("_").to!string;
     writeln(normalizedLine);
+
+    const source = "1 2 3";
+    foreach (const word; source.splitter)
+    {
+        const x = word.to!int;
+    }
+
+    auto words = source.splitter;
+    const x = words.front.to!int; words.popFront;
 }
