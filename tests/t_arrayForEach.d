86a24b43d16a88c3e5d0b608e63a0e4f4f02b422
Per Nordlöw <per.nordlow@gmail.com>
2015-02-17 23:30:05 +0100

    Add usage of partialSort

--- tests/t_arrayForEach.d
+++ tests/t_arrayForEach.d
@@ -7,4 +7,14 @@ unittest
 
     Array!int a;
     foreach (e; a[].filter!"true") {}
+
+    a ~= 1;
+    a ~= 3;
+    a ~= 2;
+    a ~= 4;
+
+    import std.algorithm.sorting: partialSort;
+    a[].partialSort(0);
+    import std.stdio;
+    writeln(a[]);
 }
