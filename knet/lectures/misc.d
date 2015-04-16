da64a8c621e43c7f5be9316a8a5e24b49bcdf484
Per Nordlöw <per.nordlow@gmail.com>
2015-04-16 21:58:22 +0200

    Fix indents

--- knet/lectures/misc.d
+++ knet/lectures/misc.d
@@ -5,9 +5,9 @@ import knet.base;
 void learnEnglishMisc(Graph graph)
 {
     graph.connectMto1(graph.add([`preserve food`,
-                                   `cure illness`,
-                                   `augment cosmetics`],
-                                  Lang.en, Sense.noun, Origin.manual),
+                                 `cure illness`,
+                                 `augment cosmetics`],
+                                Lang.en, Sense.noun, Origin.manual),
                       Role(Rel.uses),
                       graph.add(`herb`, Lang.en, Sense.noun, Origin.manual),
                       Origin.manual, 1.0);
