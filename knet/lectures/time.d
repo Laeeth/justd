ad3d86816047e60f2d666d1289f195e77de1e597
Per Nordlöw <per.nordlow@gmail.com>
2015-04-19 18:49:19 +0200

    Reuse Nd

--- knet/lectures/time.d
+++ knet/lectures/time.d
@@ -2,21 +2,24 @@ module knet.lectures.time;
 
 import knet.base;
 
-void learnTime(Graph graph)
+void learnTime(Graph gr)
 {
     writeln(`Reading Time ...`);
 
-    // TODO Reuse Node(nounSingular:weekday@en)
-    graph.learnMto1MultiLingual(Lang.en, [`Monday`, `Tuesday`, `Wednesday`, `Thursday`, `Friday`, `Saturday`, `Sunday`],    Role(Rel.instanceOf), `weekday`, Sense.weekday, Lang.en, Sense.nounSingular, 1.0);
+    enum origin = Origin.manual;
+
+    gr.learnMto1(Lang.en, [`time period`], Role(Rel.isA), `noun`, Sense.noun, Sense.nounSingular, 1.0);
+    gr.learnMto1(Lang.en, [`month`], Role(Rel.isA), `time period`, Sense.noun, Sense.nounSingular, 1.0);
+
+    const weekday = gr.add("weekday", Lang.en, Sense.timePeriod, origin);
+    const month = gr.add("month", Lang.en, Sense.timePeriod, origin);
+
+    gr.learnMto1(Lang.en, [`Monday`, `Tuesday`, `Wednesday`, `Thursday`, `Friday`, `Saturday`, `Sunday`],    Sense.weekday, Role(Rel.instanceOf), weekday, 1.0, origin);
     // TODO weekday must be in Swedish
-    graph.learnMto1MultiLingual(Lang.de, [`Montag`, `Dienstag`, `Mittwoch`, `Donnerstag`, `Freitag`, `Samstag`, `Sonntag`], Role(Rel.instanceOf), `weekday`, Sense.weekday, Lang.en, Sense.nounSingular, 1.0);
-    graph.learnMto1MultiLingual(Lang.sv, [`måndag`, `tisdag`, `onsdag`, `torsdag`, `fredag`, `lördag`, `söndag`], Role(Rel.instanceOf), `weekday`, Sense.weekday, Lang.en, Sense.nounSingular, 1.0);
+    gr.learnMto1(Lang.de, [`Montag`, `Dienstag`, `Mittwoch`, `Donnerstag`, `Freitag`, `Samstag`, `Sonntag`], Sense.weekday, Role(Rel.instanceOf), weekday, 1.0, origin);
+    gr.learnMto1(Lang.sv, [`måndag`, `tisdag`, `onsdag`, `torsdag`, `fredag`, `lördag`, `söndag`], Sense.weekday, Role(Rel.instanceOf), weekday, 1.0, origin);
 
-    // TODO Reuse Node(nounSingular:month@en)
-    graph.learnMto1MultiLingual(Lang.en, [`January`, `February`, `Mars`, `April`, `May`, `June`, `July`, `August`, `September`, `Oktober`, `November`, `December`], Role(Rel.instanceOf), `month`, Sense.month, Lang.en, Sense.nounSingular, 1.0);
-    graph.learnMto1MultiLingual(Lang.de, [`Januar`, `Februar`, `März`, `April`, `Mai`, `Juni`, `Juli`, `August`, `September`, `Oktober`, `November`, `Dezember`], Role(Rel.instanceOf), `month`, Sense.month, Lang.en, Sense.nounSingular, 1.0);
-    graph.learnMto1MultiLingual(Lang.sv, [`januari`, `februari`, `mars`, `april`, `maj`, `juni`, `juli`, `augusti`, `september`, `oktober`, `november`, `december`], Role(Rel.instanceOf), `month`, Sense.month, Lang.en, Sense.nounSingular, 1.0);
-
-    graph.learnMto1MultiLingual(Lang.en, [`time period`], Role(Rel.isA), `noun`, Sense.noun, Lang.en, Sense.nounSingular, 1.0);
-    graph.learnMto1MultiLingual(Lang.en, [`month`], Role(Rel.isA), `time period`, Sense.noun, Lang.en, Sense.nounSingular, 1.0);
+    gr.learnMto1(Lang.en, [`January`, `February`, `Mars`, `April`, `May`, `June`, `July`, `August`, `September`, `Oktober`, `November`, `December`], Sense.month, Role(Rel.instanceOf), month, 1.0, origin);
+    gr.learnMto1(Lang.de, [`Januar`, `Februar`, `März`, `April`, `Mai`, `Juni`, `Juli`, `August`, `September`, `Oktober`, `November`, `Dezember`], Sense.month, Role(Rel.instanceOf), month, 1.0, origin);
+    gr.learnMto1(Lang.sv, [`januari`, `februari`, `mars`, `april`, `maj`, `juni`, `juli`, `augusti`, `september`, `oktober`, `november`, `december`], Sense.month, Role(Rel.instanceOf), month, 1.0, origin);
 }
